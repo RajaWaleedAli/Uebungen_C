6d3c23a6b512758bc0e0fbaf9bebb3d2e4a9b62d
RajaWaleedAli <waleed.ali06@icloud.com>
2023-12-07 09:29:31 +0100

    kukuk

--- Code/GameOfLife.c
+++ Code/GameOfLife.c
@@ -14,27 +14,42 @@ int nachbar(){
 
 }
 
-void spielfeld(int spalten, int zeilen, char* feld[zeilen][spalten], char* mensch[zeilen][spalten]){
+void spielfeld(int spalten, int zeilen, char mensch[zeilen][spalten]){
     int i, j;
+    int x=0;
+    char feld[zeilen][spalten]
     for(i=0;i<zeilen, i++){
         for(j=0; j<spalten; j++){
             feld[i][j]="- ";
-            if(mensch[i][j]=="X "){
-                feld[i][j]="X ";
+            if(mensch[i][j]=='X'){
+                feld[i][j]='X';
             }
-            printf("%s")
+            x++;
+            printf("%s ", feld[i][j]);
         }
+        printf("\n");
     }
-
+    if(x==zeilen*spalten){
+        for(i=0;i<zeilen, i++){
+            for(j=0; j<spalten; j++){
+                printf("%s ", feld[i][j]);
+            }
+        printf("\n");
+        }
+        x=0;
+    }
 }
 
 int main(){
-    int i, j, life, zeilen, spalten;
+    int i, j, life, zeilen, spalten, count;
     printf("Gib die Zeilenanzahl an! ");
     scanf("%d", &zeilen);
     printf("Gib die Spaltenanzahl an! ");
     scanf("%d", &spalten);
-    char feld[zeilen][spalten];
+    printf("Gib die Anzahl der Menschen an! ");
+    scanf("%d", &count);
+    char mensch[zeilen][spalten];
+    spielfeld(spalten, zeilen)
 
     return 0;
 }
