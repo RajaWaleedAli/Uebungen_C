1e2b265e6a789261180f76a16d80999c6af0a6b2
RajaWaleedAli <waleed.ali06@icloud.com>
2023-12-11 13:09:13 +0100

    kein plan

--- Code/GameOfLife.c
+++ Code/GameOfLife.c
@@ -10,50 +10,43 @@ Beschreibung: Game of life.
 #include<stdlib.h>
 #include<math.h>
 
-int nachbar(zeilen, spalten, char mensch[zeilen][spalten], int posX, int posY){
+int nachbar(int zeilen, int  spalten, char mensch[zeilen][spalten], int posX, int posY){
     int neighbor = 0;
-    if(mensch[posY][posX]=='X'){
-        if(mensch[(posY+1+zeilen)%zeilen][posX]=='X'){
-            neighbor++;
-        }
-        if(mensch[(posY-1+zeilen)%zeilen][posX]=='X'){
-            neighbor++;
-        }
-        if(mensch[posY][(posX+1+spalten)%spalten]=='X'){
-            neighbor++;
-        }
-        if(mensch[posY][(posX-1+spalten)%spalten]=='X'){
-            neighbor++;
-        }
-        if(mensch[(posY+1+zeilen)%zeilen][(posX+1+spalten)%spalten]=='X'){
-            neighbor++;
-        }
-        if(mensch[(posY-1+zeilen)%zeilen][(posX-1+spalten)%spalten]=='X'){
-            neighbor++;
-        }
-        if(mensch[(posY-1+zeilen)%zeilen][(posX+1+spalten)%spalten]=='X'){
-            neighbor++;
-        }
-        if(mensch[(posY+1+zeilen)%zeilen][(posX-1+spalten)%spalten]=='X'){
-            neighbor++;
-        }
 
-        return neighbor;
+    if(mensch[(posY+1+zeilen)%zeilen][posX]=='X'){
+        neighbor++;
     }
+    if(mensch[(posY-1+zeilen)%zeilen][posX]=='X'){
+        neighbor++;
+    }
+    if(mensch[posY][(posX+1+spalten)%spalten]=='X'){
+        neighbor++;
+    }
+    if(mensch[posY][(posX-1+spalten)%spalten]=='X'){
+        neighbor++;
+    }
+    if(mensch[(posY+1+zeilen)%zeilen][(posX+1+spalten)%spalten]=='X'){
+        neighbor++;
+    }
+    if(mensch[(posY-1+zeilen)%zeilen][(posX-1+spalten)%spalten]=='X'){
+        neighbor++;
+    }
+    if(mensch[(posY-1+zeilen)%zeilen][(posX+1+spalten)%spalten]=='X'){
+        neighbor++;
+    }
+    if(mensch[(posY+1+zeilen)%zeilen][(posX-1+spalten)%spalten]=='X'){
+        neighbor++;
+    }
+
+    return neighbor;
 }
 
-void spielfeld(int zeilen, int spalten, char mensch[zeilen][spalten]){
+void spielfeld(int zeilen, int spalten, char mensch[zeilen][spalten], char feld[zeilen][spalten]){
     int i, j;
-    char feld[zeilen][spalten];
+    //char feld[zeilen][spalten];
+    int neighbor;
     system("cls");
-        for(i=0;i<zeilen; i++){
-            for(j=0; j<spalten; j++){
-                feld[i][j]='-';
-                if(mensch[i][j]=='X'){
-                    feld[i][j]='X';
-                }
-            }
-        }
+        
         for(i=0;i<zeilen; i++){
             for(j=0; j<spalten; j++){
                 printf("%c ", feld[i][j]);
@@ -62,7 +55,16 @@ void spielfeld(int zeilen, int spalten, char mensch[zeilen][spalten]){
         } 
         for(i=0;i<zeilen; i++){
             for(j=0; j<spalten; j++){
-                nachbar(zeilen, spalten, mensch, j, i);
+                neighbor=nachbar(zeilen, spalten, mensch, j, i);
+                if(neighbor==3){
+                    feld[i][j]='X';
+                }else if(neighbor<3){
+                    feld[i][j]='-';
+                }
+                else if(neighbor>3){
+                    feld[i][j]='-';
+                }
+                printf("%d", neighbor);                
             }
         } 
 }
@@ -78,6 +80,7 @@ int main(){
     printf("Gib die Anzahl der Menschen an! ");
     scanf("%d", &count);
     char mensch[zeilen][spalten];
+    char feld[zeilen][spalten];
 
     for(i=0;i<count; i++){
         j=rand()%zeilen;
@@ -90,11 +93,25 @@ int main(){
         
     }
 
-    spielfeld(spalten, zeilen, mensch);
-    while(input!='x'){
-
+    for(i=0;i<zeilen; i++){
+        for(j=0; j<spalten; j++){
+            feld[i][j]='-';
+            if(mensch[i][j]=='X'){
+                feld[i][j]='X';
+            }
+        }
     }
 
+    spielfeld(spalten, zeilen, mensch, feld);
+    int loop = 1;
+    while(loop=1){
+        input=getch();
+        if(input=='x'){
+            return 0;
+        }else{
+            spielfeld(spalten, zeilen, mensch, feld);
+        }
+    }
 
     return 0;
 }
