0dc66ed39054d968af50fcd6b6245dc74a83ed83
RajaWaleedAli <waleed.ali06@icloud.com>
2023-12-11 12:18:50 +0100

    ausgabe funktioniert

--- Code/GameOfLife.c
+++ Code/GameOfLife.c
@@ -10,38 +10,34 @@ Beschreibung: Game of life.
 #include<stdlib.h>
 #include<math.h>
 
-int nachbar(){
-
+int nachbar(zeilen, spalten, char mensch[zeilen]){
+    if()
 }
 
-void spielfeld(int spalten, int zeilen, char mensch[zeilen][spalten]){
+void spielfeld(int zeilen, int spalten, char mensch[zeilen][spalten]){
     int i, j;
-    int x=0;
-    char feld[zeilen][spalten]
-    for(i=0;i<zeilen, i++){
-        for(j=0; j<spalten; j++){
-            feld[i][j]="- ";
-            if(mensch[i][j]=='X'){
-                feld[i][j]='X';
-            }
-            x++;
-            printf("%s ", feld[i][j]);
-        }
-        printf("\n");
-    }
-    if(x==zeilen*spalten){
-        for(i=0;i<zeilen, i++){
+    char feld[zeilen][spalten];
+    system("cls");
+        for(i=0;i<zeilen; i++){
             for(j=0; j<spalten; j++){
-                printf("%s ", feld[i][j]);
+                feld[i][j]='-';
+                if(mensch[i][j]=='X'){
+                    feld[i][j]='X';
+                }
+            }
+        }
+        for(i=0;i<zeilen; i++){
+            for(j=0; j<spalten; j++){
+                printf("%c ", feld[i][j]);
             }
         printf("\n");
-        }
-        x=0;
-    }
+        } 
 }
 
 int main(){
-    int i, j, life, zeilen, spalten, count;
+    srand(time(NULL));
+    int i, j, k, life, zeilen, spalten, count;
+    char input;
     printf("Gib die Zeilenanzahl an! ");
     scanf("%d", &zeilen);
     printf("Gib die Spaltenanzahl an! ");
@@ -49,7 +45,23 @@ int main(){
     printf("Gib die Anzahl der Menschen an! ");
     scanf("%d", &count);
     char mensch[zeilen][spalten];
-    spielfeld(spalten, zeilen)
+
+    for(i=0;i<count; i++){
+        j=rand()%zeilen;
+        k=rand()%spalten;
+        if(mensch[j][k]=='X'){
+            i--;
+        }else{
+           mensch[j][k]='X'; 
+        }
+        
+    }
+
+    spielfeld(spalten, zeilen, mensch);
+    while(input!='x'){
+
+    }
+
 
     return 0;
 }
