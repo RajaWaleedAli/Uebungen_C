a54d7e796423d256baa56eccb07dae4443134710
RajaWaleedAli <waleed.ali06@icloud.com>
2024-02-05 13:08:15 +0100

    ChatsStats funktioniert -d aber name einlesen ist komisch deshalb chatStatsV2

--- Code/chatStatsV2.c
+++ Code/chatStatsV2.c
@@ -11,15 +11,6 @@ Zu Beachten: Funktioniert nur wenn der kontankt Eingespeichert ist und wenn es m
 #include<string.h>
 #include <stdbool.h>
 
-int namen(int temp, int temp2){
-    if(temp2==']'&&temp!=':'){
-        return temp;
-    }else if(temp==':'){
-        return ';';
-    } 
-    return 0;
-}
-
 int main(int argc, char* argv[]){
     system("cls");
     int i, temp, temp2, count;
@@ -29,6 +20,7 @@ int main(int argc, char* argv[]){
     char name2[30000];
     char *name3[3000]={0};
     char* ptr;
+    bool first = false; 
     bool leertaste = false; 
     i=0;
     FILE *fp;
@@ -38,55 +30,30 @@ int main(int argc, char* argv[]){
         printf("Datei konnte NICHT geoeffnet werden.\n");
         return 404;
     }
+    int j=0;
     //Namen im Array speichern Anfang
-    while((temp = fgetc(fp))!=EOF){
-        if(temp2==']'){
-            name[i]=namen(temp, temp2);
-            //printf("%c", namen(temp, temp2));
-            i++;
+    while(fgets(temp3, 30000, fp)!=NULL){
+        printf("+%s+\n", strchr(temp3, (int)']'));
+        printf("#%s#\n", strtok(strchr(temp3, (int)']'), ":"));
+        fflush(stdout);
+        getchar();
+        ptr = strtok(strchr(temp3, (int)']'), ":");
+        while(first==false){
+            name[j]=ptr;
+            j++;
         }
-        if(temp==']'){
-            temp2=']';
-        }else if(temp==':'){
-            temp2=':';
+        if(strstr(name3, ptr)==NULL){
+            strcpy(name3[j], ptr);
+            j++;
         }
-        
-    }
-    ptr = strtok(name, ";");
-    while(ptr != NULL) {
-        if(strstr(name2, ptr)==NULL){
-            strcat(name2, ptr);
-        } 
-        ptr = strtok(NULL, ";");
-    }
-    count=0;
-    for(i=0; name2[i]!='\0'; i++){
-        if(name2[i]==' ' && leertaste==false){
-            leertaste=true;
-            count++;
-        }else if(name2[i]==' ' && leertaste==true){
-            leertaste=false;
-            if(count>0){
-                name2[i]=';';
-            }
-            
-            printf("\n");
-            count++;
+        for(i=0; name3[i]!=NULL; i++){
+            printf("%d: %s\n", i, name3[i]);
         }
-        //printf("%c", name2[i]);    
     }
     int stats[100]={0};
     fclose(fp);
     //Namen im Array Speichern Ende
 
-    ptr = strtok(name2, ";");
-    i=0;
-    while(ptr != NULL) {
-        //printf( " %s\n", ptr);
-        name3[i]=ptr;
-        ptr = strtok(NULL, ";");
-        i++;
-    }
     fp = fopen("_chat.txt", "r");
 
     char* platzhalter;
