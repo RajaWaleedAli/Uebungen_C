0ac86479ce58adb2d3491186e4df6a6cad50eaa1
RajaWaleedAli <waleed.ali06@icloud.com>
2024-04-17 15:16:57 +0200

    kukuk

--- Code/TelefonbuchListe.c
+++ Code/TelefonbuchListe.c
@@ -14,22 +14,22 @@ typedef struct knoten{
     struct knoten* next;
 }knoten_t;
 
-void addKnot(knoten_t *, char *, long long);
+void addKnot(knoten_t **, char *, long long);
 //void delKnot(struct *);
 //void searchKnot(struct *);
 void printList(knoten_t *);
 
 int main(){
-    knoten_t* head;
+    knoten_t* head = (knoten_t *) malloc(sizeof(knoten_t));
     head=NULL;
     char namen[30];
     long long nummer;
     for(int i=0;i<5;i++){
         printf("\nGib Name ein! ");
-        scanf("%s", namen);
+        scanf("%s", &namen);
         printf("\nGib Nummer ein! ");
-        scanf("%lld", nummer);
-        addKnot(head, namen, nummer);
+        scanf("%lld", &nummer);
+        addKnot(&head, namen, nummer);
     }
     printList(head);
     return 0;
@@ -44,27 +44,25 @@ void printList(struct knoten* head){
     }
 }
 
-void addKnot(struct knoten* head, char name[30], long long nummer){
-    knoten_t* searchPtr = head;
+void addKnot(struct knoten** head, char namen[30], long long nummer){
+    knoten_t* searchPtr = *head;
     knoten_t* new_Knot = (knoten_t *) malloc(sizeof(knoten_t));
-    strcpy(new_Knot->name, name);
+    strcpy(new_Knot->name, namen);
     new_Knot->number = nummer;
     new_Knot->next = NULL;
     int i=0;
-    if(head==NULL || strcmp(name, searchPtr->name) < 0){
-        *new_Knot->next = *head;
-        *head = *new_Knot;
+    if(*head==NULL || strcmp(namen, searchPtr->name) < 0){
+        new_Knot->next = *head;
+        *head = new_Knot;
         return;
     }
     while(searchPtr->next!=NULL){
-        if(searchPtr->next->name[0]<new_Knot->name[0]){
-            *new_Knot->next = *searchPtr;
-            *searchPtr = *new_Knot;
-            return;
-        }else if(searchPtr->next->next==NULL){
-            searchPtr = (knoten_t *) malloc(sizeof(knoten_t));
-            *searchPtr->next->next = *new_Knot;
+        if(strcmp(namen, searchPtr->next->name) < 0){
+            new_Knot->next = searchPtr->next;
+            searchPtr->next = new_Knot;
             return;
         }
+        searchPtr = searchPtr->next;
     }
+    searchPtr->next=new_Knot;
 }
